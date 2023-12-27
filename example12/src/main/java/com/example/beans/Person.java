a76bd2dbf01510226e710da010d2554ab7e07c40
Gitesh Kumar <gitesh.kumar2503@gmail>
2023-12-27 13:06:03 +0530

    Autowiring ways (Default is by Type but if this fails then byName is Done. Have listed ways to do autowiring byname)

---
+++ example12/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,40 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name="Lucy";
+    private final Vehicle vehicle;
+
+//    NOT recommended WAY
+//    @Autowired
+//    public Person( Vehicle vehicle1){
+//        System.out.println("Person bean created by Spring");
+//        this.vehicle = vehicle1;
+//    }
+
+
+    // RECOMMENDED WAY(Fav topic in interviews ie. ofr @Qualifier("") annotation uses....)
+    @Autowired
+    public Person( @Qualifier("vehicle2") Vehicle vehicle){
+        System.out.println("Person bean created by Spring");
+        this.vehicle = vehicle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+}
---
+++ example12/src/main/java/com/example/main/Example12.java
@@ -0,0 +1,23 @@
+package com.example.main;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Example12 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        System.out.println("Person name from Spring Context is: " + person.getName());
+        System.out.println("Vehicle that Person own is: " + person.getVehicle());
+
+    }
+}
