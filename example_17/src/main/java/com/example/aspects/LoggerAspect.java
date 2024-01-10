f28987334c40f4e3cebf80023b343654ef6046ff
Gitesh Kumar <gitesh.kumar2503@gmail>
2024-01-10 22:59:04 +0530

    Added Aspect Oriented Programming (AOP) Concepts.

---
+++ example_17/src/main/java/com/example/aspects/LoggerAspect.java
@@ -0,0 +1,58 @@
+package com.example.aspects;
+
+import com.example.services.VehicleServices;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Aspect       // Mandatory annotation for AOP
+@Component    // Mandatory annotation for AOP
+@Order(2)     // Defines order of the aspect, ie. order in which AOP logic will run...
+public class LoggerAspect {
+
+    private Logger logger = Logger.getLogger(LoggerAspect.class.getName());
+
+    @Around("execution(* com.example.services.*.*(..))")
+    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info(joinPoint.getSignature().toString() + " method execution start");
+        Instant start = Instant.now();
+        joinPoint.proceed(); // this will tell spring framework that we are done with out before logic
+                             // and now execute the actual method, and now give back the control to me
+                             // to execute the after logic.......
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        logger.info("Time took to execute the method : "+timeElapsed);
+        logger.info(joinPoint.getSignature().toString() + " method execution end");
+    }
+
+    @Around("@annotation(com.example.interfaces.LogAspect)")
+    public void logWithAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info(joinPoint.toString() + " method execution start");
+        Instant start = Instant.now();
+        joinPoint.proceed();
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        logger.info("Time took to execute the method : "+timeElapsed);
+        logger.info(joinPoint.getSignature().toString() + " method execution end");
+    }
+
+    @AfterThrowing(value = "execution(* com.example.services.*.*(..))",throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        logger.log(Level.SEVERE,joinPoint.getSignature()+ " An exception thrown with the help of" +
+                " @AfterThrowing which happened due to : "+ex.getMessage());
+    }
+
+    @AfterReturning(value = "execution(* com.example.services.*.*(..))",returning = "retVal")
+    public void logStatus(JoinPoint joinPoint,Object retVal) {
+        logger.log(Level.INFO,joinPoint.getSignature()+ " Method successfully processed with the status " +
+                retVal.toString());
+    }
+}
